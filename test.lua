b380bdc62afb4572042ee7d1891de278b82a9e99
DamBer00593 <99778771+DamBer00593@users.noreply.github.com>
2022-02-16 18:42:28 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -7,23 +7,16 @@ This program uses DaBeAPI.lua (not fully integrated)
 local component = require("component")
 local sides = require("sides")
 local DaBeAPI = require("\hdd\DaBeAPI.lua")
-
-local side = 5
-local slot = 1
+local InvComp = component.inventory_controller
 
 for i = 5,0,-1 do
-  print(i)
-  local item = component.inventory_controller.getStackInSlot(i,1)
- 
+  slot = 1
+  local item = DaBeAPI.inv_getSlotInfo(InvComp,i,slot)
+
   if item then
     print("Chest on side: "..i)
-    --print(slot..": ItemName("..item.name.."), itemLabel("..item.label.."), itemQuantity("..item.size..")")
-    
-    print(DaBeAPI.toString(1,item))
+    print(DaBeAPI.toString(slot,item))
   else
      print("Side "..i.." Has no chest")
   end
 end
- 
- 
---component.inventory_controller.dropIntoSlot(sides.west,1,1)
