dbd91bacb1937b69e131b8ebbb9aa68a4e0e1803
DamBer00593 <99778771+DamBer00593@users.noreply.github.com>
2022-02-16 18:28:57 -0400

    Update autoUpdate.lua

--- autoUpdate.lua
+++ autoUpdate.lua
@@ -1,3 +1,4 @@
 os.execute("rm test.lua")
+os.execute("rm DaBeAPI.lua")
 os.execute("wget https://raw.githubusercontent.com/DamBer00593/DDSS-OS-Demo/main/test.lua")
 os.execute("wget https://raw.githubusercontent.com/DamBer00593/DDSS-OS-Demo/main/DaBeAPI.lua")
