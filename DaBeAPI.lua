8c3c8c3d4dec7373b7ea9e7917e154fcf01a531a
DamBer00593 <99778771+DamBer00593@users.noreply.github.com>
2022-02-16 13:17:35 -0400

    Create DaBeAPI.lua

---
+++ DaBeAPI.lua
@@ -0,0 +1,16 @@
+--[[
+DaBeAPI V1.0 OpenCC Version
+Computercraft version can be found on pastebin at https://pastebin.com/dN6PjadH
+Made by DaBe00593, and assisted by xAssassin321
+Last updated 2022-02-16 (year/month/day)
+--]]
+
+local DaBeAPI = {}
+local version = 1.0
+  function DaBeAPI.getVersion()
+    return version
+  end
+  
+  
+
+return DaBeAPI
