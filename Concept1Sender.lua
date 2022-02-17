0eb92c2184b6900bed51510dc1ee07c84fb69bf3
DamBer00593 <99778771+DamBer00593@users.noreply.github.com>
2022-02-17 10:54:33 -0400

    Update Concept1Sender.lua

--- Concept1Sender.lua
+++ Concept1Sender.lua
@@ -14,13 +14,17 @@ local InvComp = component.inventory_controller
 local modem = component.modem
 local chestSide = 5
 local slot = 1
+local itemName = thermalfoundation:material
+local itemDmg = 352
 --End Of Instance Variable Declaration--
 
 while true do
   local item = DaBeAPI.invGetSlotInfo(invComp,chestSide,slot)
   if item then --In this statement we verify if the item is nil or not if it is we print error to the terminal
     
-    if item.name == minecraft:dirt and item.dmg == 0 then --In this statement we determine if the name and the dmg value of the item match whats expected
+    --The variables itemName and itemDmg are purely for testing purposes the names can be hardcoded in at a later date!
+    
+    if item.name == itemName and itemDmg == 0 then --In this statement we determine if the name and the dmg value of the item match whats expected
       
       if item.size == 16 then --We verify the quantity is correct
         modem.broadcast(9,"test")
